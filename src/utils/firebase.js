1d97ceda6d4e99d9abdf4135ed454de8f2a6fb5d
grela31 <jesusvarelagrela1999@gmail.com>
2022-05-18 19:43:14 +0200

    Añadir firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuVLhl44NsaHTM5L8L0ydyNxc38eZoVBc",
+  authDomain: "peliculas-jesus.firebaseapp.com",
+  projectId: "peliculas-jesus",
+  storageBucket: "peliculas-jesus.appspot.com",
+  messagingSenderId: "400362932028",
+  appId: "1:400362932028:web:b1d66c54a631c9caa8c039"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
